5733660b381dcc90ea557cb36c8be150f131dfed
kipade <kipade@163.com>
2024-02-20 13:48:50 +0800

    r13462 - CC: code refactoring step 6: member variable name changes

--- src/plugins/codecompletion/ccoptionsdlg.cpp
+++ src/plugins/codecompletion/ccoptionsdlg.cpp
@@ -2,8 +2,8 @@
  * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
  * http://www.gnu.org/licenses/gpl-3.0.html
  *
- * $Revision: 13461 $
- * $Id: ccoptionsdlg.cpp 13461 2024-02-20 02:37:10Z ollydbg $
+ * $Revision: 13462 $
+ * $Id: ccoptionsdlg.cpp 13462 2024-02-20 02:37:21Z ollydbg $
  * $HeadURL: https://svn.code.sf.net/p/codeblocks/code/trunk/src/plugins/codecompletion/ccoptionsdlg.cpp $
  */
 
@@ -84,7 +84,7 @@ BEGIN_EVENT_TABLE(CCOptionsDlg, wxPanel)
 END_EVENT_TABLE()
 
 CCOptionsDlg::CCOptionsDlg(wxWindow* parent, ParseManager* np, CodeCompletion* cc, DocumentationHelper* dh) :
-    m_NativeParser(np),
+    m_ParseManager(np),
     m_CodeCompletion(cc),
     m_Parser(np->GetParser()),
     m_Documentation(dh)
@@ -157,8 +157,8 @@ CCOptionsDlg::CCOptionsDlg(wxWindow* parent, ParseManager* np, CodeCompletion* c
     XRCCTRL(*this, "chkComplexMacros",      wxCheckBox)->SetValue(m_Parser.Options().parseComplexMacros);
     XRCCTRL(*this, "chkPlatformCheck",      wxCheckBox)->SetValue(m_Parser.Options().platformCheck);
 
-    XRCCTRL(*this, "rdoOneParserPerWorkspace", wxRadioButton)->SetValue( m_NativeParser->IsParserPerWorkspace());
-    XRCCTRL(*this, "rdoOneParserPerProject",   wxRadioButton)->SetValue(!m_NativeParser->IsParserPerWorkspace());
+    XRCCTRL(*this, "rdoOneParserPerWorkspace", wxRadioButton)->SetValue( m_ParseManager->IsParserPerWorkspace());
+    XRCCTRL(*this, "rdoOneParserPerProject",   wxRadioButton)->SetValue(!m_ParseManager->IsParserPerWorkspace());
 
     // Page "Symbol browser"
     XRCCTRL(*this, "chkInheritance",        wxCheckBox)->SetValue(m_Parser.ClassBrowserOptions().showInheritance);
@@ -284,7 +284,7 @@ void CCOptionsDlg::OnApply()
 
     // Now write the parser options and re-read them again to make sure they are up-to-date
     m_Parser.WriteOptions();
-    m_NativeParser->RereadParserOptions();
+    m_ParseManager->RereadParserOptions();
     m_Documentation->WriteOptions(cfg);
     m_CodeCompletion->RereadOptions();
 }

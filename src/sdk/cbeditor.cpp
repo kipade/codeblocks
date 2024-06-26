d8443e87ca0409f0bedec46ee32e1bceed3bb732
kipade <kipade@163.com>
2024-06-26 14:05:37 +0800

    r13531 | mortenmacfly - reduce compiler warnings
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13531 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/sdk/cbeditor.cpp
+++ src/sdk/cbeditor.cpp
@@ -2,8 +2,8 @@
  * This file is part of the Code::Blocks IDE and licensed under the GNU Lesser General Public License, version 3
  * http://www.gnu.org/licenses/lgpl-3.0.html
  *
- * $Revision: 13521 $
- * $Id: cbeditor.cpp 13521 2024-05-15 15:52:54Z pecanh $
+ * $Revision: 13531 $
+ * $Id: cbeditor.cpp 13531 2024-06-21 20:24:30Z mortenmacfly $
  * $HeadURL: https://svn.code.sf.net/p/codeblocks/code/trunk/src/sdk/cbeditor.cpp $
  */
 
@@ -291,8 +291,7 @@ struct cbEditorInternalData
         }
     }
 
-    static int CalcWidth(cbStyledTextCtrl* control, int baseWidth, int minWidth,
-                         float defaultPointSize)
+    static int CalcWidth(cbStyledTextCtrl* control, int baseWidth, int minWidth, float defaultPointSize)
     {
         int width = baseWidth * (defaultPointSize + control->GetZoom()) / defaultPointSize;
         if (width < minWidth)
@@ -306,12 +305,12 @@ struct cbEditorInternalData
         const float pointSize = m_pOwner->m_pControl->StyleGetFont(wxSCI_STYLE_DEFAULT).GetPointSize();
         if (both)
         {
-            const int width = CalcWidth(m_pOwner->m_pControl, baseWidth, minWidth, pointSize);
-            m_pOwner->m_pControl->SetMarginWidth(marginId, width);
+            const int width_ctrl_1 = CalcWidth(m_pOwner->m_pControl, baseWidth, minWidth, pointSize);
+            m_pOwner->m_pControl->SetMarginWidth(marginId, width_ctrl_1);
             if (m_pOwner->m_pControl2)
             {
-                const int width = CalcWidth(m_pOwner->m_pControl2, baseWidth, minWidth, pointSize);
-                m_pOwner->m_pControl2->SetMarginWidth(marginId, width);
+                const int width_ctrl_2 = CalcWidth(m_pOwner->m_pControl2, baseWidth, minWidth, pointSize);
+                m_pOwner->m_pControl2->SetMarginWidth(marginId, width_ctrl_2);
             }
         }
         else
@@ -859,7 +858,7 @@ public:
         SetDataObject(dataobj);
     }
 
-    wxDragResult OnData(wxCoord x, wxCoord y, wxDragResult defaultDragResult) override
+    wxDragResult OnData(wxCoord x, wxCoord y, cb_unused wxDragResult defaultDragResult) override
     {
         GetData();
         wxDataObjectComposite *dataobjComp = static_cast<wxDataObjectComposite *>(GetDataObject());

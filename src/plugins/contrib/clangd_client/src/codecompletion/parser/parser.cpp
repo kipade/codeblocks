6a95efa60d95c1dc783b56586e41e0634502a27e
kipade <kipade@163.com>
2025-07-08 10:44:40 +0800

    r13638 | wh11204 - Clangd_plugin: Fixed compilation with wxWidgets 3.3.0 again. Reapply [r13622], removed by [r13636].
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13638 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/plugins/contrib/clangd_client/src/codecompletion/parser/parser.cpp
+++ src/plugins/contrib/clangd_client/src/codecompletion/parser/parser.cpp
@@ -2,8 +2,8 @@
  * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
  * http://www.gnu.org/licenses/gpl-3.0.html
  *
- * $Revision: 13636 $
- * $Id: parser.cpp 13636 2025-03-20 17:25:57Z pecanh $
+ * $Revision: 13638 $
+ * $Id: parser.cpp 13638 2025-03-22 09:21:54Z wh11204 $
  * $HeadURL: https://svn.code.sf.net/p/codeblocks/code/trunk/src/plugins/contrib/clangd_client/src/codecompletion/parser/parser.cpp $
  */
 
@@ -1567,7 +1567,7 @@ void Parser::ShowGlobalChangeAnnoyingMsg()
                        "on the project title in the Workspace tree and selecting\n"
                        "'Reparse current project'.");
 
-        AnnoyingDialog dlg(_("Global settings warning"), _(warningMsg), wxART_WARNING,
+        AnnoyingDialog dlg(_("Global settings warning"), warningMsg, wxART_WARNING,
                            AnnoyingDialog::OK);
         dlg.ShowModal();
     }//endif size

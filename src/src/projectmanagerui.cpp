26412adb4af6a04ba33beb693bd4d8a452e9e184
kipade <kipade@163.com>
2025-07-08 10:48:02 +0800

    r13650 | wh11204 * Remove assertion when deleting a directory containing removed files (ticket #1525, thanks LETARTARE).
    git-svn-id: https://svn.code.sf.net/p/codeblocks/code/trunk@13650 2a5c6006-c6dd-42ca-98ab-0921f2732cef

--- src/src/projectmanagerui.cpp
+++ src/src/projectmanagerui.cpp
@@ -2,8 +2,8 @@
  * This file is part of the Code::Blocks IDE and licensed under the GNU General Public License, version 3
  * http://www.gnu.org/licenses/gpl-3.0.html
  *
- * $Revision: 13626 $
- * $Id: projectmanagerui.cpp 13626 2025-03-02 17:53:46Z mortenmacfly $
+ * $Revision: 13650 $
+ * $Id: projectmanagerui.cpp 13650 2025-04-06 09:40:37Z wh11204 $
  * $HeadURL: https://svn.code.sf.net/p/codeblocks/code/trunk/src/src/projectmanagerui.cpp $
  */
 
@@ -1205,8 +1205,24 @@ void ProjectManagerUI::RemoveFilesRecursively(wxTreeItemId& sel_id)
                 cbProject* prj = ftd->GetProject();
                 if (prj && ftd->GetKind() == FileTreeData::ftdkFile)
                 {
-                    if ( ProjectFile* pf = ftd->GetProjectFile() )
-                        Manager::Get()->GetProjectManager()->RemoveFileFromProject(pf, prj);
+                    ProjectFile* pf = ftd->GetProjectFile();
+                    if (pf)
+                    {
+                        wxString dir = pf->file.GetPath();
+                        if(::wxDirExists(dir))
+                        {
+                            Manager::Get()->GetProjectManager()->RemoveFileFromProject(pf, prj);
+                        }
+                        else
+                        {
+                            wxString msg = wxString::Format(_("The directory '%s' no longer exists on disk") , dir);
+                            msg += "\n\n" + _("Instead, use 'Remove files...' and select all") + " ...";
+                            cbMessageBox(msg, _("Error"), wxICON_ERROR);
+                            Manager::Get()->GetLogManager()->LogError(msg);
+
+                            break;
+                        }
+                    }
                 }
                 else if (  ftd->GetKind() == FileTreeData::ftdkFolder
                         || ftd->GetKind() == FileTreeData::ftdkVirtualFolder)
